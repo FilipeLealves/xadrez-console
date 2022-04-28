0fa973e096c08d34ab3256fa2f1737e97a83c812
FilipeLealves <lealves.work@gmail.com>
2022-04-28 10:30:24 -0300

    Comecando a implementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace xadrez_console
+﻿using tabuleiro;
+
+namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Position p = new Position(3, 4);
 
+            Console.WriteLine("Posição: "+ p);
         }
     }
 }
---
+++ xadrez-console/tabuleiro/Piece.cs
@@ -0,0 +1,10 @@
+﻿namespace tabuleiro
+{
+    internal class Piece
+    {
+        public Position position { get; set; }
+        public Color color { get; protected set; }
+        public int numberMoves { get; protected set; }
+        public Chessboard cb { get; protected set; }
+    }
+}
